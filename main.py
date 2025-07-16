bd8896fbc6f21ad4418896b4068f7de864945a57
HayfordVI <brighthffrd@gmail.com>
2025-07-16 23:46:17 +0000

    rock_paper_scissors

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-import random
+import random as rd
 
 while True:
-    player = input("Roll the dice? (y/n): ")
-    if player.casefold() == "y":
-        answer = random.randrange(1, 6)
-        answero = random.randrange(1, 6)
-        print(f'({answero}, {answer})')
-    elif player.casefold() == "n":
-        print("Thanks for playing!")
+    user_choice = input('Rock, paper, scissors? (r/p/s) ')
+
+    game_choices = ["r", "p", "s"]
+    opponent_choice = (rd.choice(game_choices))
+
+    if user_choice == 'r' and opponent_choice == 'p':
+        print('You chose 🪨')
+        print('The opps chose 📃')
+        print("you lose")
+    elif user_choice == 'r' and opponent_choice == 's':
+        print('You chose 🪨')
+        print('The opps chose ✂️')
+        print('You win')
+    elif user_choice == 'p' and opponent_choice == 'r':
+        print('You chose 📃')
+        print('The opps chose 🪨')
+        print('You win')
+    elif user_choice == 'p' and opponent_choice == 's':
+        print('You chose 📃')
+        print('The opps chose ✂️')
+        print('You lose')
+    elif user_choice == 's' and opponent_choice == 'p':
+        print('You chose ✂️')
+        print('The opps chose 📃')
+        print('You win')
+    elif user_choice == 's' and opponent_choice == 'r':
+        print('You chose ✂️')
+        print('The opps chose 🪨')
+        print('You lose')
+    elif user_choice == opponent_choice:
+        print('It is a tie')
+    else:
+        print('Invalid choice')
+
+    after_game = input('Continue? (y/n) ')
+    if after_game == 'y':
+        pass
+    elif after_game == 'n':
         break
     else:
-        print('Invalid choice!')
+        print('Invalid choice')
+
+# the program must pick one at random
+# must program the hierarchy
+# the option to terminate the game after one round
